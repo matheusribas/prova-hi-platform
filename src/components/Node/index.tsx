28d345b005eced7871e681825f98ee374e37bd61
Matheus Ribas <matheusfeliperibas@gmail.com>
2024-03-14 14:36:59 -0300

    feat: accordion creation for child elements

--- src/components/Node/index.tsx
+++ src/components/Node/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { ChevronDown } from "lucide-react"
 import { DataType } from "../../types/DataType"
 
@@ -6,23 +7,43 @@ interface NodeProps {
 }
 
 export function Node({ node }: NodeProps) {
+  const [openAccordions, setOpenAccordions] = useState<{[id: string]: boolean}>({})
+
+  function handleToggleAccordion(nodeId: string) {
+    setOpenAccordions(prev => {
+      const updatedOpenAccordions = { ...prev }
+      updatedOpenAccordions[nodeId] = !updatedOpenAccordions[nodeId]
+      
+      return { ...updatedOpenAccordions }
+    })
+  }
+
   return (
     Object.entries(node).map(([, nodeCurrent]) => {
       return (
-        <ul key={nodeCurrent.id} className="w-full bg-slate-700 rounded overflow-hidden">
+        <ul key={nodeCurrent.id} className="w-full bg-slate-700 hover:bg-slate-700/70 first:rounded-t last:rounded-b overflow-hidden">
           <li className="flex justify-between items-center gap-4 border-b border-slate-600">
             <div className="flex gap-4 flex-1 items-center">
               <input id={nodeCurrent.id} type="checkbox" className="accent-green-500 w-4 h-4 ml-4" />
               <label htmlFor={nodeCurrent.id} className="font-semibold flex-1 py-4 cursor-pointer">{nodeCurrent.name}</label>
             </div>
             {(!!Object.keys(nodeCurrent.children).length) && (
-              <button className="pr-4 flex-1 flex justify-end">
-                <ChevronDown />
+              <button 
+                className="pr-4 flex-1 flex justify-end"
+                onClick={_ => handleToggleAccordion(nodeCurrent.id)}
+              >
+                <ChevronDown 
+                  data-accordion={openAccordions[nodeCurrent.id] ? 'open' : 'closed'}
+                  className="transition-transform data-[accordion='open']:rotate-180" 
+                />
               </button>
             )}
           </li>
           {(Object.keys(nodeCurrent.children).length !== 0) && (
-            <li className="pl-8">
+            <li
+              className="pl-8 max-h-0 data-[accordion='open']:max-h-full"
+              data-accordion={openAccordions[nodeCurrent.id] ? 'open' : 'closed'}
+            >
               <Node node={nodeCurrent.children} />
             </li>
           )}
